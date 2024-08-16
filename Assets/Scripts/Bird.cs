c4539330cef7cd83840cf524835ca7ace4aa6898
gil1min <gilbert1min@gmail.com>
2024-08-16 10:37:28 +0800

    first commit

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public enum BirdState
+{
+	Waiting,
+	BeforeShoot,
+	AfterShoot
+}
+public class Bird : MonoBehaviour
+{
+	public BirdState state = BirdState.BeforeShoot;
+	private bool isMouseDown = false;
+	public float maxDistance = 3f;
+	public float flySpeed = 15;
+	private Rigidbody2D rgd;
+	// Start is called before the first frame update
+	void Start()
+	{
+		rgd = GetComponent<Rigidbody2D>();
+		rgd.bodyType = RigidbodyType2D.Static;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		switch (state)
+		{
+			case BirdState.BeforeShoot:
+				MoveControl();
+				break;
+			default:
+				break;
+		}
+	}
+
+	private void OnMouseDown()
+	{
+		if (state == BirdState.BeforeShoot)
+		{
+			isMouseDown = true;
+			Slingshot.Instance.StartDraw(transform);
+		}
+	}
+
+	private void OnMouseUp()
+	{
+		if (state == BirdState.BeforeShoot)
+		{
+			isMouseDown = false;
+			Slingshot.Instance.EndDraw();
+			Fly();
+		}
+	}
+
+	private void MoveControl()
+	{
+		if (isMouseDown) {
+			transform.position = GetMousePostion();
+		}
+	}
+
+	private Vector3 GetMousePostion()
+	{
+		Vector3 centerPosition = Slingshot.Instance.GetCenterPosition();
+		Vector3 mousePosition =  Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mousePosition.z = 0;
+		Vector3 mouseDirection = mousePosition - centerPosition;
+		float distance = mouseDirection.magnitude;
+		if (distance > maxDistance) {
+			mousePosition = mouseDirection.normalized * maxDistance + centerPosition;
+		}
+		return mousePosition;
+	}
+
+	private void Fly()
+	{
+		rgd.bodyType = RigidbodyType2D.Dynamic;
+		rgd.velocity = (Slingshot.Instance.GetCenterPosition() - transform.position).normalized * flySpeed;
+		state = BirdState.AfterShoot;
+	}
+}
---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+	public static Slingshot Instance { get; private set; }
+	private LineRenderer leftRenderer;
+	private LineRenderer rightRenderer;
+	private Transform leftPoint;
+	private Transform rightPoint;
+	private Transform centerPoint;
+	private bool isDrawing = false;
+	private Transform birdTransform;
+	private void Awake()
+	{
+		Instance = this;
+	}
+	// Start is called before the first frame update
+	void Start()
+	{
+		leftRenderer = transform.Find("LeftLine").GetComponent<LineRenderer>();
+		rightRenderer = transform.Find("RightLine").GetComponent<LineRenderer>();
+		leftPoint = transform.Find("LeftPoint");
+		rightPoint = transform.Find("RightPoint");
+		centerPoint = transform.Find("CenterPoint");
+
+		HideLine();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (isDrawing)
+		{
+			Draw();
+		}
+	}
+
+	public void StartDraw(Transform birdTransform)
+	{
+		isDrawing = true;
+		this.birdTransform = birdTransform;
+		ShowLine();
+	}
+
+	public void EndDraw()
+	{
+		isDrawing = false;
+		HideLine();
+	}
+
+	public void Draw()
+	{
+		Vector3 birdPosition = birdTransform.position;
+		birdPosition = (birdPosition - centerPoint.position).normalized * 0.4f + birdPosition;
+		leftRenderer.SetPosition(0, birdPosition);
+		leftRenderer.SetPosition(1, leftPoint.position);
+		rightRenderer.SetPosition(0, birdPosition);
+		rightRenderer.SetPosition(1, rightPoint.position);
+	}
+
+	public Vector3 GetCenterPosition()
+	{
+		return centerPoint.transform.position;
+	}
+
+	private void HideLine() 
+	{
+		rightRenderer.enabled = false;
+		leftRenderer.enabled = false;
+	}
+
+	private void ShowLine() 
+	{
+		rightRenderer.enabled = true;
+		leftRenderer.enabled = true;
+	}
+}
